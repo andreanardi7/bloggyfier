1ae3e01e30ee1b2505c047e5e1d2eb43aebff4af
andreanardi7 <volleygamer123@gmail.com>
2023-11-01 16:04:38 +0100

    Updated pattern logic and connected HTML to the Js

--- front-end/model.js
+++ front-end/model.js
@@ -1,7 +1,7 @@
 let fileName;
 
 // Generatore Nome del file
-function generateFile(){
+function generateFileName(){
     // Generate randdom string
     function generateRandomAlphanumericString(length) {
         const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -11,29 +11,10 @@ function generateFile(){
             const randomIndex = Math.floor(Math.random() * characters.length);
             alphanumericString += characters.charAt(randomIndex);
         }
-  
-        return alphanumericString;
+        
+        // Generate a random alphanumeric string of a specific length
+        const length = 10; 
+        const randomAlphanumericString = generateRandomAlphanumericString(length);
+        return ("blog_" + randomAlphanumericString + ".html");
     }
-  
-  // Usage: generate a random alphanumeric string of a specific length
-  const length = 10; // Adjust the length as needed
-  const randomAlphanumericString = generateRandomAlphanumericString(length);
-
-  // Concat the generated code to the standard naming convention
-  fileName.writeFile("blog_".concat(randomAlphanumericString,".md"), "", (err) => {
-    if (err) throw err;
-  });
-}
-
-// writing of the snippet
-function writeFile(fileName) {
-    // i truncate the file name extension with slice(0, -3) 
-    let id = fileName.replace('.md',"")
-    var snippet = 
-    '<div>'+
-        '<span id = ' + id + '>'+
-    '</div>'+
-    '<div>'+
-        '<script src="https://www.bloggyfier.com/index.js?id="' + id + '>'+
-    '</div>'
 }
