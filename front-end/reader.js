2c0f81ce77fbf399ebf18829e06cd0c88367fea8
andreanardi7 <volleygamer123@gmail.com>
2023-11-08 23:41:22 +0100

    New Snippet handler and new sidebar and colors

--- front-end/reader.js
+++ front-end/reader.js
@@ -1,1 +1,15 @@
 // Use API GET to read the file from file name
+const queryString = window.location.search;
+const onlyLettersPattern = /^[A-Za-z]+$/;
+const validTags = ["html", "txt"];
+
+// Printing even if not accepted just for testing reasons
+console.log(queryString);
+
+if (validTags.includes(userQuery)) {
+    // Useing a filter to remove bad characters that coul cause an SQL Injection
+    if (queryString.match(onlyLettersPattern)) {
+        const urlParams = new URLSearchParams(queryString);
+        readFile(queryString);
+    }
+}
